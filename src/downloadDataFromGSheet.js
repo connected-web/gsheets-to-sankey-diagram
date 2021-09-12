ab8eb309979f7ccb53d78c4f7b58617142d4bc87
John Beech <johnbeech.github@mkv25.net>
2021-09-12 22:08:18 +0100

    Get the raw data from google sheets using javascript

---
+++ src/downloadDataFromGSheet.js
@@ -0,0 +1,41 @@
+const gsjson = require('google-spreadsheet-to-json')
+const readCredentials = require('./util/readCredentials')
+const spreadsheets = [{
+  name: 'Finances - Tag Analysis',
+  id: '1H6sD9XQVuLQTvoKkoJ0IjZ9cgbwRGacVuSPLk9ZTq1E'
+}]
+const { write } = require('promise-path')
+
+const log = []
+const report = (...messages) => console.log(['[Download Data from Google Spreadsheet]', ...messages].join(' '))
+
+async function downloadData ({ gsjson }, { name, id }) {
+  report('Downloading', name, 'using id:', id)
+  const spreadsheetId = id
+  try {
+    const credentials = await readCredentials()
+    const worksheets = await gsjson({ spreadsheetId, allWorksheets: true, credentials })
+    report('Downloaded data:', (worksheets + '').length, 'bytes')
+    const cells = worksheets.reduce((acc, item) => acc.concat(item), [])
+    report(name, 'cells', cells.length)
+    return cells
+  } catch (ex) {
+    report('Unable to download data:', ex)
+  }
+}
+
+async function downloadSpreadsheets (fetchers, spreadsheets) {
+  const asyncWork = spreadsheets.map(spreadsheet => {
+    return downloadData(fetchers, spreadsheet)
+  })
+  const allSheets = await Promise.all(asyncWork)
+
+  const combinedSheets = allSheets.reduce((acc, item) => acc.concat(item), [])
+
+  await write('downloaded-data.json', JSON.stringify(combinedSheets, null, 2), 'utf8')
+
+  return combinedSheets
+}
+
+const fetchers = { gsjson }
+downloadSpreadsheets(fetchers, spreadsheets)
